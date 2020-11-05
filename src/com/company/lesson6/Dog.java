dbc75c02dba5313c74e56588b3fc3420fccb90d3
litvakivan <litvakivan@gmail.com>
2020-11-05 22:42:48 +0300

    //Home work 6 is complete!

--- src/com/company/lesson6/Dog.java
+++ src/com/company/lesson6/Dog.java
@@ -2,32 +2,17 @@ package com.company.lesson6;
 
 public class Dog extends Animal {
 
-    private final int lengthRun;
-    private final int lengthSwim;
-    private final double heightJump;
+    private int lengthSwim;
 
     public Dog() {
-        this.lengthRun = ((int) (Math.random() * 50 + 450));
-        this.lengthSwim = ((int) (Math.random() * 10));
-        this.heightJump = Math.random() * 5 / 10;
+        lengthRun = ((int) (Math.random() * 50 + 450));
+        lengthSwim = ((int) (Math.random() * 10));
+        heightJump = Math.random() * 5 / 10;
     }
-
 
     public boolean swim(int length) {
         System.out.println(length + " " + (length <= lengthSwim));
         return length <= lengthSwim;
-    }
-
-    @Override
-    public boolean run(int length) {
-        System.out.println(length + " " + (length <= lengthRun));
-        return length <= lengthRun;
-    }
-
-    @Override
-    public boolean jump(double height) {
-        System.out.println(height + " " + (height <= heightJump));
-        return height <= heightJump;
     }
 
     @Override
@@ -39,15 +24,5 @@ public class Dog extends Animal {
                 '}';
     }
 
-    public int getLengthRun() {
-        return lengthRun;
-    }
 
-    public int getLengthSwim() {
-        return lengthSwim;
-    }
-
-    public double getHeightJump() {
-        return heightJump;
-    }
 }
