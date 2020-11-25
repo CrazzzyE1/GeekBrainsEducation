c014a74f4515dd89dd7ae04817db7f3258e28f73
litvakivan <litvakivan@gmail.com>
2020-11-25 19:10:04 +0300

    //Home work JAVA 2 // Lesson 5 Chat Design

--- JAVA2/src/company/lesson4/ChatApplication.java
+++ JAVA2/src/company/lesson4/ChatApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ChatApplication extends Application {
@@ -11,7 +12,8 @@ public class ChatApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent auth = FXMLLoader.load(getClass().getResource("fxml/auth.fxml"));
-        primaryStage.setTitle("Авторизация");
+        primaryStage.setTitle("Welcome to the GeekChat");
+        primaryStage.getIcons().add(new Image("company/lesson4/icon.png"));
         primaryStage.setScene(new Scene(auth));
         primaryStage.setResizable(false);
         primaryStage.show();
