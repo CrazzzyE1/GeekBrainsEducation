bcc846289408a411b6961111a81fd8b87c33cb1b
litvakivan <litvakivan@gmail.com>
2020-12-27 14:29:24 +0300

    chat bugfix

--- JAVA2/src/company/lesson7/Client.java
+++ JAVA2/src/company/lesson7/Client.java
@@ -19,9 +19,10 @@ public class Client implements Closeable {
     private byte[] buffer;
     private int bytesRead;
     private boolean flag;
+    Thread readThread;
 
     public static Client getInstance() throws IOException {
-        if(instance == null) {
+        if (instance == null) {
             instance = new Client();
         }
         return instance;
@@ -38,40 +39,54 @@ public class Client implements Closeable {
 
     public void write(String msg) {
 
-            try {
-                out.write(msg.getBytes(StandardCharsets.UTF_8));
-                out.flush();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            out.write(msg.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     public boolean isAuthSuccess() throws IOException {
+        System.out.println("Внутри авторизации на клиенте");
         bytesRead = in.read(buffer);
+        System.out.println("Внутри авторизации на клиенте");
         String msg = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
+        System.out.println("Ответ от сервера при авторизации: " + msg);
         return msg.equals("/success");
     }
 
+    public void readThreadStop(){
+        flag = false;
+        System.out.println("TreadStop Flag = false");
+    }
+    public void readThreadStart(){
+        flag = true;
+        System.out.println("ThreadStart");
+        readThread.start();
+    }
+
     public void read(TextArea output) throws IOException {
-        Thread readThread = new Thread(() -> {
+        System.out.println(flag);
+        readThread = new Thread(() -> {
             while (flag) {
+                System.out.println("Я есть поток чтения");
                 try {
                     bytesRead = in.read(buffer);
                     String msg = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
+                    System.out.println("Поток на клиенте вход сообщение: " + msg);
                     output.appendText(msg);
                 } catch (IOException e) {
-                    System.out.println("User leave!");;
+                    System.out.println("User leave!");
                 }
-
             }
         });
-        readThread.start();
     }
 
     @Override
     public void close() throws IOException {
-        flag= false;
+        flag = false;
         in.close();
         out.close();
     }
